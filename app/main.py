d6435c5a2d5cbe3f63338b8d2054814b90be28d2
tonybnya <tonybnya@gmail.com>
2026-05-18 10:43:03 +0100

    style+chore: restructure the app & setup tailwind

--- app/main.py
+++ app/main.py
@@ -1,10 +1,14 @@
 from datetime import datetime
 from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 from fastapi.responses import HTMLResponse
 from fastapi import FastAPI, Request
 
 # define a FastAPI app
 app = FastAPI()
+
+# mount the 'static' folder to be accessible via /static
+app.mount('/static', StaticFiles(directory='static'), name='static')
 
 # define the templates folder
 templates = Jinja2Templates(directory='templates')
@@ -20,7 +24,7 @@ def health():
 
 
 @app.get('/index', response_class=HTMLResponse)
-def index(request: Request):
+async def index(request: Request):
     return templates.TemplateResponse(
         request=request,
         name='index.html',
